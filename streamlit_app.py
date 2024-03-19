9884ddecd686ebe3856a663f085bf88689b62eb0
MPS7 <MPS7@users.noreply.github.com>
2024-03-19 00:05:39 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,52 @@
 import streamlit as st
+import numpy as np
+import time
 
-st.write("COVID-19 Impact in El Paso County, TX")
 
-high = st.radio(
+st.header("COVID-19 Impact in El Paso County, TX")
+
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    variant = st.radio(
     "Select variant features 👉",
-    key="visibility",
+    key="variant",
     options=["Highly transmissible", "Immune escaping", "Both"],
-)
+    )
+
+with col2:
+    vacc = st.radio(
+    "Select booster vaccination coverage 👉",
+    key="vacc",
+    options=["30 %", "10 %", "0 %"],
+    )
+
+with col3:
+    #st.write("UTEP Mathematical Sciences Department")
+    st.image("utep.jpg")
+    
+
+
+
+
+
+
+
+print(variant)
+
+
+
+
+st.button("Reset", type="primary")
+if st.button('Run simulation'):
+    with st.spinner('Wait for it...'):
+    	time.sleep(5)
+    	st.success('Done!')
+
+    data = np.random.randn(10, 1)
+
+    st.subheader("A wide column with a chart")
+    st.line_chart(data)
+else:
+    st.write('Goodbye')
