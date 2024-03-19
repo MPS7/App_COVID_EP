6725fd35fedc00a26c5fe14bfbd128318196cab8
MPS7 <MPS7@users.noreply.github.com>
2024-03-18 22:48:12 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,10 @@ import streamlit as st
 number = st.slider("Pick a number", 0, 100)
 
 st.write("You're scheduled for:", number)
+
+
+st.button("Reset", type="primary")
+if st.button('Say hello'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
